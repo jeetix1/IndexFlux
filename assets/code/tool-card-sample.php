923cdb3a14c4e32a114497d307e2f5f3d5b56b65
Jeetix <jeetix@gmail.com>
2024-11-09 01:20:20 +0000

    even more logical maybe

---
+++ assets/code/tool-card-sample.php
@@ -0,0 +1,9 @@
+<div class="tool-card" data-categories="code" onclick="location.href='./category/toolname'">
+  <div class="tool-icon">🏷️</div>
+  <div class="tool-name">Tool Name</div>
+  <div class="tool-description">Short description of the tool's functionality, ideally not much more than one sentence.</div>
+  <div class="tool-tags">
+    <span class="tool-tag">HTML</span>
+    <span class="tool-tag">CSS</span>
+  </div>
+</div>
